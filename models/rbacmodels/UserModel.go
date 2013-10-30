c088bc2f088f535d9fa7a5402a2eb4d2a3a20203
刘志铭 <osgochina@gmail.com>
2013-10-30 23:12:27 +0800

    update

---
+++ models/rbacmodels/UserModel.go
@@ -0,0 +1,112 @@
+package rbacmodels
+
+import (
+	"admin/lib"
+	"errors"
+	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/validation"
+	"log"
+	"time"
+)
+
+//用户表
+type User struct {
+	Id            int
+	Username      string    `orm:"unique;size(32)" form:"Username"  valid:"Required;MaxSize(20);MinSize(6)"`
+	Password      string    `orm:"size(32)" form:"Password" valid:"Required;MaxSize(20);MinSize(6)"`
+	Repassword    string    `orm:"-" form:"Repassword" valid:"Required"`
+	Nickname      string    `orm:"unique;size(32)" form:"Nickname" valid:"Required;MaxSize(20);MinSize(2)"`
+	Email         string    `orm:"size(32)" form:"Email" valid:"Email"`
+	Remark        string    `orm:"null;size(200)" form:"Remark" valid:"MaxSize(200)"`
+	Status        int       `orm:"default(2)" form:"Status" valid:"Range(1,2)"`
+	Lastlogintime time.Time `orm:"null;type(datetime)" form:"-"`
+	Createtime    time.Time `orm:"type(datetime);auto_now_add" `
+	Role          []*Role   `orm:"rel(m2m)"`
+}
+
+func (u *User) TableName() string {
+	return "user"
+}
+
+func (u *User) Valid(v *validation.Validation) {
+	if u.Password != u.Repassword {
+		v.SetError("Repassword", "两次输入的密码不一样")
+	}
+}
+
+//验证用户信息
+func checkUser(u *User) (err error) {
+	valid := validation.Validation{}
+	b, _ := valid.Valid(&u)
+	if !b {
+		for _, err := range valid.Errors {
+			log.Println(err.Key, err.Message)
+			return errors.New(err.Message)
+		}
+	}
+	return nil
+}
+
+//添加用户
+func AddUser(u *User) (int64, error) {
+	if err := checkUser(u); err != nil {
+		return 0, err
+	}
+	o := orm.NewOrm()
+	user := new(User)
+	user.Username = u.Username
+	user.Password = lib.Strtomd5(u.Password)
+	user.Nickname = u.Nickname
+	user.Email = u.Email
+	user.Remark = u.Remark
+	user.Status = u.Status
+
+	id, err := o.Insert(user)
+	return id, err
+}
+
+//更新用户
+func UpdateUser(u *User) (int64, error) {
+	if err := checkUser(u); err != nil {
+		return 0, err
+	}
+	o := orm.NewOrm()
+	user := make(orm.Params)
+	if len(u.Username) > 0 {
+		user["Username"] = u.Username
+	}
+	if len(u.Nickname) > 0 {
+		user["Nickname"] = u.Nickname
+	}
+	if len(u.Email) > 0 {
+		user["Email"] = u.Email
+	}
+	if len(u.Remark) > 0 {
+		user["Remark"] = u.Remark
+	}
+	if u.Status != 0 {
+		user["Status"] = u.Status
+	}
+	if len(user) == 0 {
+		return 0, errors.New("update field is empty")
+	}
+
+	num, err := o.QueryTable("user").Filter("Id", u.Id).Update(user)
+	return num, err
+}
+
+/************************************************************/
+func init() {
+	orm.RegisterModel(new(User))
+}
+
+//get user list
+func Getuserlist(where string, page int, page_size int, sort string) (users []orm.Params, count int64) {
+	o := orm.NewOrm()
+	user := new(User)
+	qs := o.QueryTable(user)
+	qs.Limit(0, 10)
+	qs.Values(&users)
+	count, _ = qs.Count()
+	return users, count
+}
