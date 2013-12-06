cdc63919d54ac3c1c0f41c9c80ba1aa5869b0b50
刘志铭 <osgochina@gmail.com>
2013-12-06 22:12:13 +0800

    add the postgres database support

--- src/rbac/common.go
+++ src/rbac/common.go
@@ -33,10 +33,17 @@ func init() {
 	db_user := beego.AppConfig.String("db_user")
 	db_pass := beego.AppConfig.String("db_pass")
 	db_name := beego.AppConfig.String("db_name")
+	db_sslmode := beego.AppConfig.String("db_sslmode")
 	switch db_type {
 	case "mysql":
+		orm.RegisterDriver("mysql", orm.DR_MySQL)
 		dns = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", db_user, db_pass, db_host, db_port, db_name)
 		break
+	case "postgres":
+		orm.RegisterDriver("postgres", orm.DR_Postgres)
+		dns = fmt.Sprintf("dbname=%s host=%s  user=%s  password=%s  port=%s  sslmode=%s", db_name, db_host, db_user, db_pass, db_port, db_sslmode)
+	default:
+		beego.Critical("Database driver is not allowed:", db_type)
 	}
 	orm.RegisterDataBase("default", db_type, dns)
 	//验证权限
